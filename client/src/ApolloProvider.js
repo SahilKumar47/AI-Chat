a6455859ef8d3d7d517083f3f90968d76f811bba
Sahil <sahilkumarpcm@gmail.com>
2020-10-15 21:41:01 +0530

    add get user query and markup

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -2,11 +2,28 @@ import React from "react";
 import {
   ApolloClient,
   InMemoryCache,
+  createHttpLink,
   ApolloProvider as Provider,
 } from "@apollo/client";
 
-const client = new ApolloClient({
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
   uri: "http://localhost:4000",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
